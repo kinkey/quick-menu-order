169e3f7528cb66b753e4ae40be56ab64898f62d0
kinkey <george.sandu.grigorescu@gmail.com>
2018-12-18 12:19:17 +0200

    initial project structure

---
+++ src/main/java/com/quickorder/QuickorderApplication.java
@@ -0,0 +1,14 @@
+package com.quickorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickorderApplication.class, args);
+    }
+
+}
+
